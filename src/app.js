ac48d87ae358fb5bf11e9cef35cfe3a3caeb9c60
Devendra Reddy Mekala <devendrareddym23@gmail.com>
2026-03-15 18:15:55 -0500

    Allow Vercel frontend in CORS

--- src/app.js
+++ src/app.js
@@ -1,21 +1,11 @@
-const express = require("express");
 const cors = require("cors");
-const explainRoute = require("./routes/explainRoute");
-const screenshotRoutes = require("./routes/screenshotRoutes");
-const errorHandler = require("./middlewares/errorHandler");
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  return res.send("API is running");
-});
-
-app.use("/api/explain", explainRoute);
-app.use("/api/screenshots", screenshotRoutes);
-
-app.use(errorHandler);
-
-module.exports = app;
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://explain-this-screenshot-jdlfns186.vercel.app",
+    ],
+    methods: ["GET", "POST"],
+  })
+);
