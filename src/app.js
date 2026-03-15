6aa4c28a38d87c469cd2dcf1b80e70b271505364
Devendra Reddy Mekala <devendrareddym23@gmail.com>
2026-03-15 18:28:01 -0500

    Fix screenshot routes import

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
+const express = require("express");
 const cors = require("cors");
+
+const screenshotRoutes = require("./routes/screenshotRoutes");
+
+const app = express();
 
 app.use(
   cors({
@@ -9,3 +14,14 @@ app.use(
     methods: ["GET", "POST"],
   })
 );
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("AI Screenshot Explainer API is running");
+});
+
+app.use("/api/screenshots", screenshotRoutes);
+
+module.exports = app;
