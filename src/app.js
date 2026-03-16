bb1702ef0a95ab3b78e85a464c9ea85d09b560ca
Devendra Reddy Mekala <devendrareddym23@gmail.com>
2026-03-15 21:10:26 -0500

    fix cors for frontend

--- src/app.js
+++ src/app.js
@@ -7,10 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://explain-this-screenshot-jdlfns186.vercel.app",
-    ],
+    origin: true,
     methods: ["GET", "POST"],
   })
 );
